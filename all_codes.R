4f322ec28df93a73fc7391aaa1e4ff840ab9da4d
Aryan Giri <144023908+programmer-03@users.noreply.github.com>
2023-12-20 11:00:30 +0530

    Update all_codes.R

--- all_codes.R
+++ all_codes.R
@@ -11,7 +11,7 @@ for (num in numbers) {
 
 
 
-#2# Calculate sum, average, minimum, and maximum of the numbers divisible by both 2 and 3
+# Calculate sum, average, minimum, and maximum of the numbers divisible by both 2 and 3
 sum_result <- sum(result)
 average_result <- mean(result)
 minimum_result <- min(result)
@@ -21,6 +21,30 @@ print(paste("Sum of numbers divisible by both 2 and 3:", sum_result))
 print(paste("Average of numbers divisible by both 2 and 3:", average_result))
 print(paste("Minimum of numbers divisible by both 2 and 3:", minimum_result))
 print(paste("Maximum of numbers divisible by both 2 and 3:", maximum_result))
+
+
+#2
+# Combine logical conditions and perform calculations
+numbers <- 1:10
+result <- c()
+
+for (num in numbers) {
+  if (num %% 2 == 0 & num %% 3 == 0) {
+    result <- c(result, num)
+  }
+}
+
+# Calculate sum, average, minimum, and maximum of the numbers divisible by both 2 and 3
+sum_result <- sum(result)
+average_result <- mean(result)
+minimum_result <- min(result)
+maximum_result <- max(result)
+
+print(paste("Sum of numbers divisible by both 2 and 3:", sum_result))
+print(paste("Average of numbers divisible by both 2 and 3:", average_result))
+print(paste("Minimum of numbers divisible by both 2 and 3:", minimum_result))
+print(paste("Maximum of numbers divisible by both 2 and 3:", maximum_result))
+
 
 
 
